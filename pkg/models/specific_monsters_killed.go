ebd574e7c4968ea2979aebbab88aed15721f6f90
Sarah Vessels <cheshire137@gmail.com>
2019-11-09 17:06:27 -0600

    Add Counts to SpecificMonstersKilled

--- pkg/models/specific_monsters_killed.go
+++ pkg/models/specific_monsters_killed.go
@@ -10,6 +10,14 @@ type SpecificMonstersKilled struct {
 	Items   []Item   `xml:"item"`
 }
 
+func (m *SpecificMonstersKilled) Counts() map[string]int {
+	counts := make(map[string]int, len(m.Items))
+	for _, item := range m.Items {
+		counts[item.Key.String] = item.Value.Int
+	}
+	return counts
+}
+
 func (m *SpecificMonstersKilled) String() string {
 	var sb strings.Builder
 	for _, item := range m.Items {
