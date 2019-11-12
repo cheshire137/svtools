790c85d8c92d0827130b70271e13d575a3adf303
Sarah Vessels <cheshire137@gmail.com>
2019-11-11 19:10:10 -0600

    Add failing assertion for matching original save

--- pkg/models/save_file_test.go
+++ pkg/models/save_file_test.go
@@ -19,6 +19,10 @@ func TestSaveFile_CanSaveAsXmlFile(t *testing.T) {
 	err := saveFile.Load()
 	require.NoError(t, err)
 
+	// Read the original contents
+	originalContents, err := saveFile.Read()
+	require.NoError(t, err)
+
 	// Write a copy of the save file elsewhere on disk
 	dupePath := "tmp-can-save-duplicate-test.xml"
 	err = saveFile.Save(dupePath, false)
@@ -31,4 +35,25 @@ func TestSaveFile_CanSaveAsXmlFile(t *testing.T) {
 	dupeFile := NewSaveFile(dupePath)
 	err = dupeFile.Load()
 	require.NoError(t, err)
+
+	// Should match the original contents of the save file
+	dupeContents, err := dupeFile.Read()
+	require.NoError(t, err)
+
+	saveTestArtifact(originalContents, "test-output/original-save.xml", t)
+	saveTestArtifact(dupeContents, "test-output/duplicate-save.xml", t)
+
+	require.Equal(t, originalContents, dupeContents)
+}
+
+func saveTestArtifact(contents string, path string, t *testing.T) {
+	if _, err := os.Stat(path); err == nil {
+		err := os.Remove(path)
+		require.NoError(t, err)
+	}
+	file, err := os.Create(path)
+	require.NoError(t, err)
+	defer file.Close()
+	file.WriteString(contents)
+	file.Sync()
 }
