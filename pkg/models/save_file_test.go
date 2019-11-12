eb0ae11c47049acb6abbe6ea05a2120b00b96c41
Sarah Vessels <cheshire137@gmail.com>
2019-11-11 18:43:39 -0600

    Add test for loading a duplicate save

--- pkg/models/save_file_test.go
+++ pkg/models/save_file_test.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,5 +10,25 @@ import (
 func TestSaveFile_LoadsFromXmlPath(t *testing.T) {
 	saveFile := NewSaveFile("fixtures/Anne_130176584")
 	err := saveFile.Load()
+	require.NoError(t, err)
+}
+
+func TestSaveFile_CanSaveAsXmlFile(t *testing.T) {
+	// Load a Stardew Valley save
+	saveFile := NewSaveFile("fixtures/Anne_130176584")
+	err := saveFile.Load()
+	require.NoError(t, err)
+
+	// Write a copy of the save file elsewhere on disk
+	dupePath := "tmp-can-save-duplicate-test.xml"
+	err = saveFile.Save(dupePath, false)
+	require.NoError(t, err)
+	defer func() {
+		os.Remove(dupePath)
+	}()
+
+	// Should be able to load that copy without error
+	dupeFile := NewSaveFile(dupePath)
+	err = dupeFile.Load()
 	require.NoError(t, err)
 }
